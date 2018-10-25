c2cc1ba6d0503f826d6fa0a5ff7fd00e8246c81d
Elias Balena Mazzocco <elias.mazzocco@gmail.com>
2018-10-25 18:12:46 -0300

    Binding expression to bind to variables exposed by the CalculatorViewModel.

--- TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModel.kt
+++ TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModel.kt
@@ -1,34 +1,49 @@
 package com.eliasmazz.tipcalculator.viewmodel
 
+import android.app.Application
 import android.databinding.BaseObservable
 import android.util.Log
+import com.eliasmazz.tipcalculator.R
 import com.eliasmazz.tipcalculator.model.RestaurantCalculator
 import com.eliasmazz.tipcalculator.model.TipCalculation
 
-class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalculator()) : BaseObservable(){
+class CalculatorViewModel(val app: Application, val calculator: RestaurantCalculator = RestaurantCalculator()) : BaseObservable() {
 
     private val TAG = CalculatorViewModel::class.java.simpleName
     var inputCheckAmount = ""
     var inputTipPercentage = ""
-    var tipCalculation = TipCalculation()
+
+    var outputCheckAmount = ""
+    var outputTipAmount = ""
+    var outputTotalDollarAmount = ""
+
+    init {
+        updateOutputs(TipCalculation())
+    }
+
+    private fun updateOutputs(tc: TipCalculation){
+        outputCheckAmount = app.getString(R.string.dollar_amount,tc.checkAmount)
+        outputTipAmount = app.getString(R.string.dollar_amount,tc.tipAmount)
+        outputTotalDollarAmount = app.getString(R.string.dollar_amount,tc.grandTotal)
+    }
 
     fun calculateTip() {
 
-        Log.d(TAG,"calculateTipInvoked")
+        //Log.d(TAG, "calculateTipInvoked")
 
         val checkAmount = inputCheckAmount.toDoubleOrNull();
         val tipPct = inputTipPercentage.toIntOrNull()
 
         if (checkAmount != null && tipPct != null) {
 
-            Log.d(TAG,"CheckAmount: $checkAmount, TipPercentage: $tipPct")
-            tipCalculation = calculator.calculateTip(checkAmount, tipPct)
-            cleanInputs();
+            //Log.d(TAG, "CheckAmount: $checkAmount, TipPercentage: $tipPct")
+            updateOutputs(calculator.calculateTip(checkAmount, tipPct))
+            cleanInputs()
         }
 
     }
 
-    fun cleanInputs(){
+    fun cleanInputs() {
         inputCheckAmount = "0.00"
         inputTipPercentage = "0"
         notifyChange()
--- TipCalculator/app/src/test/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModelTest.kt
+++ TipCalculator/app/src/test/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.eliasmazz.tipcalculator.viewmodel
 
+import android.app.Application
+import com.eliasmazz.tipcalculator.R
 import com.eliasmazz.tipcalculator.model.RestaurantCalculator
 import com.eliasmazz.tipcalculator.model.TipCalculation
 import junit.framework.Assert.assertEquals
@@ -17,10 +19,18 @@ class CalculatorViewModelTest {
     @Mock
     lateinit var mockRestaurantCalculator: RestaurantCalculator
 
+    @Mock
+    lateinit var application: Application
+
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
-        calculatorViewModel = CalculatorViewModel(mockRestaurantCalculator)
+        stubResource(0.0,"$0.00")
+        calculatorViewModel = CalculatorViewModel(application,mockRestaurantCalculator)
+    }
+
+    private fun stubResource(given: Double, returnStub: String) {
+        `when`(application.getString(R.string.dollar_amount,given)).thenReturn(returnStub)
     }
 
     @Test
@@ -32,9 +42,18 @@ class CalculatorViewModelTest {
         val stub = TipCalculation(checkAmount = 10.00, tipAmount = 1.5, grandTotal = 11.5)
         `when`(mockRestaurantCalculator.calculateTip(10.00,15)).thenReturn(stub)
 
+
+        stubResource(10.0,"$10.00")
+        stubResource(1.5,"$1.50")
+        stubResource(11.5,"$11.50")
+
         calculatorViewModel.calculateTip()
 
-        assertEquals(stub, calculatorViewModel.tipCalculation)
+
+        assertEquals("$10.00", calculatorViewModel.outputCheckAmount)
+        assertEquals("$1.50", calculatorViewModel.outputTipAmount)
+        assertEquals("$11.50", calculatorViewModel.outputTotalDollarAmount)
+
 
     }
 
