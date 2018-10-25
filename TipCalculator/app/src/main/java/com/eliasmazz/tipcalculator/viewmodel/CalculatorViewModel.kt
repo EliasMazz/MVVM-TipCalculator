9747cbf9c89436edf432eded3798ac2d3ffefa51
Elias Balena Mazzocco <elias.mazzocco@gmail.com>
2018-10-25 14:44:17 -0300

    Two way bind the Edit texts from Tip Calculator Content to a ViewModel backed variable.

--- TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModel.kt
+++ TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/viewmodel/CalculatorViewModel.kt
@@ -1,10 +1,11 @@
 package com.eliasmazz.tipcalculator.viewmodel
 
+import android.databinding.BaseObservable
 import android.util.Log
 import com.eliasmazz.tipcalculator.model.RestaurantCalculator
 import com.eliasmazz.tipcalculator.model.TipCalculation
 
-class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalculator()) {
+class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalculator()) : BaseObservable(){
 
     private val TAG = CalculatorViewModel::class.java.simpleName
     var inputCheckAmount = ""
@@ -18,9 +19,19 @@ class CalculatorViewModel(val calculator: RestaurantCalculator = RestaurantCalcu
         val checkAmount = inputCheckAmount.toDoubleOrNull();
         val tipPct = inputTipPercentage.toIntOrNull()
 
-        if (checkAmount != null && tipPct != null)
-            tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+        if (checkAmount != null && tipPct != null) {
 
+            Log.d(TAG,"CheckAmount: $checkAmount, TipPercentage: $tipPct")
+            tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            cleanInputs();
+        }
+
+    }
+
+    fun cleanInputs(){
+        inputCheckAmount = "0.00"
+        inputTipPercentage = "0"
+        notifyChange()
     }
 
 }
