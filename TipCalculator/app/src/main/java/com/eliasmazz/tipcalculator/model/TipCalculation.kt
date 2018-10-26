a2d9f8064eb8cd917c9b55e12cc8fc4b38907318
Elias Balena Mazzocco <elias.mazzocco@gmail.com>
2018-10-25 21:54:39 -0300

    Create a repository to save Tip Calculation object in to the memory

--- TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/model/TipCalculation.kt
+++ TipCalculator/app/src/main/java/com/eliasmazz/tipcalculator/model/TipCalculation.kt
@@ -1,6 +1,7 @@
 package com.eliasmazz.tipcalculator.model
 
 data class TipCalculation(
+    val locationName: String = "",
     val checkAmount: Double = 0.0,
     val tipPctInput: Int = 0,
     val tipAmount: Double = 0.0,
